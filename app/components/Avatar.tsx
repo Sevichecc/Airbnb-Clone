2afb9c4b904e3d943a370b1cded38dd57a04c7a2
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-28 19:28:52 +0800

    feat: Navbar

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+const Avatar = () => {
+  return (
+    <Image
+      height='30'
+      width='30'
+      className='rounded-full'
+      alt='avatar'
+      src='/images/placeholder.jpg'
+    />
+  )
+}
+
+export default Avatar
