7314c7ba7b86e21e124d40a257c02be3b380b127
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2023-05-04 13:17:03 +0800

    feat:login with Google & Github

--- app/components/navbar/Narbar.tsx
+++ app/components/navbar/Narbar.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { User } from '@prisma/client'
+import { SafeUser } from '@/app/types'
 import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
 
 interface NarbarProps {
-  currentUser?: User | null
+  currentUser?: SafeUser | null
 }
 
 const Narbar: React.FC<NarbarProps> = ({ currentUser }) => {
@@ -19,7 +19,7 @@ const Narbar: React.FC<NarbarProps> = ({ currentUser }) => {
           <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,7 +6,14 @@ import MenuItem from './MenuItem'
 import useRegisterModal from '@/app/hooks/useRegisterModal'
 import useLoginModal from '@/app/hooks/useLoginModal'
 
-const UserMenu = () => {
+import { SafeUser } from '@/app/types'
+import { signOut } from 'next-auth/react'
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal()
   const loginModal = useLoginModal()
   const [isOpen, setIsOpen] = useState(false)
@@ -27,16 +34,28 @@ const UserMenu = () => {
         >
           <AiOutlineMenu />
           <div className='hidden md:block'>
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
       {isOpen && (
         <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
-          <>
-            <MenuItem onClick={loginModal.onOpen} label='Login' />
-            <MenuItem onClick={registerModal.onOpen} label='Sign Up' />
-          </>
+          {currentUser ? (
+            <>
+              <MenuItem onClick={() => {}} label='My trips' />
+              <MenuItem onClick={() => {}} label='My favorites' />
+              <MenuItem onClick={() => {}} label='My reservations' />
+              <MenuItem onClick={() => {}} label='My properties' />
+              <MenuItem onClick={() => {}} label='Airbnb my home' />
+              <hr />
+              <MenuItem onClick={() => signOut()} label='Logout' />
+            </>
+          ) : (
+            <>
+              <MenuItem onClick={loginModal.onOpen} label='Login' />
+              <MenuItem onClick={registerModal.onOpen} label='Sign Up' />
+            </>
+          )}
         </div>
       )}
     </div>
