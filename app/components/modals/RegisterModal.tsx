8a0e6fe1b0d8e3a404b276a886b9ff97ed8887ee
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2023-05-12 02:39:55 +0800

    feat: favorite page

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -37,7 +37,10 @@ const RegisterModal = () => {
     setIsloading(true)
     axios
       .post('/api/register', data)
-      .then(() => registerModal.onClose())
+      .then(() => {
+        registerModal.onClose()
+        loginModal.onOpen()
+      })
       .catch((error) => toast.error('Something went wrong'))
       .finally(() => setIsloading(false))
   }
